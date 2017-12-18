1fc3861b8e80051c78783057399d9e197ffd6919
jyliu <cqwsljy@163.com>
2017-12-18 21:57:47 +0800

    update

--- HS300/htfgetHS300.py
+++ HS300/htfgetHS300.py
@@ -4,4 +4,18 @@ Created on Mon Dec 18 21:26:33 2017
 
 @author: Alpha
 """
+'''
+函数功能说明：
+利用wind获取沪深300历史成份股，包括成份股张跌幅和权重
 
+wind说明：
+    用到的wind内部函数有:w.wsd,w.wset
+
+
+
+
+'''
+
+
+from WindPy import w
+w.start()
