0d7f3df64cd97228d64ae723f22eead5c55ea839
Jiayong <cqwsljy@163.com>
2017-12-19 13:23:06 +0800

    update

--- HS300/htfgetHS300.py
+++ HS300/htfgetHS300.py
@@ -16,6 +16,26 @@ wind说明：
 
 '''
 
-
+import pandas as pd
 from WindPy import w
 w.start()
+
+windcode = '000300.SH'
+daystart = '2005-04-01'
+dayend = '2017-12-19'
+daystrade = w.tdays(daystart,dayend)
+
+
+#data = w.wsd('000001.SZ','close',dayend,dayend)
+
+dfhs300code = pd.DataFrame()
+dfhs300weight = pd.DataFrame()
+
+for dayt in daystrade:
+    ## 获取当日沪深300成分股及权重
+    date_code = 'date=' + dayt + ';windcode=' + windcode
+    w_wset_data = w.wset('indexconstituent',date_code)
+    dfwset = pd.DataFrame(w_wset_data.Data).T
+    dfwset.columns = w_wset_data.Fields
+
+
